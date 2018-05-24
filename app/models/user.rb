2e59abc96bfebca9eec9aa85d2b8c78a2020cf88
m <m@debian-dev-base>
2018-05-24 19:18:26 -0300

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   validates_presence_of :name
   validates :name, length: { maximum: 50 }
 
@@ -12,9 +13,26 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }
   has_secure_password
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token_param)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token_param)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,11 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 end
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+end
